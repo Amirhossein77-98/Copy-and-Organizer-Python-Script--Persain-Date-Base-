9be4144adaa2c20c3efc94c5262f6d0b938e9e75
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-01-27 16:30:41 +0330

    Seperated Frames

--- view/gui.py
+++ view/gui.py
@@ -13,22 +13,24 @@ class GUIArchitect():
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
+        self.root.rowconfigure(2, weight=1)
         self.root.minsize(600, 400)
         self.root.maxsize(600, 400)
+        title_frame = Frame(self.root)
+        title_frame.grid(column=0, row=0, pady=0)
         input_frame = Frame(self.root)
-        input_frame.grid(column=0, row=0)
-        self.add_labels(input_frame)
-        self.add_buttons(input_frame)
+        input_frame.grid(column=0, row=1)
         output_frame = Frame(self.root, bg="black")
-        output_frame.grid(column=0, row=1)
-        title = Label(output_frame, text="Copy Script")
+        output_frame.grid(column=0, row=2)
+        self.title_frame_config(title_frame)
+        self.input_frame_config(input_frame)
+        self.output_frame_config(output_frame)
+
+    def title_frame_config(self, frame):
+        title = Label(frame, text="Copy Script", font=("Times New Roman", 25))
         title.grid(sticky="EW")
 
-    def add_labels(self, frame):
-        title = Label(frame, text="Copy Script")
-        title.grid(sticky="EW")
-
-    def add_buttons(self, frame):
+    def input_frame_config(self, frame):
         choose_origin_btn = Button(frame, text="Choose Source", command=CopyModel.origin_picker)
         choose_origin_btn.grid(sticky="EW")
         choose_destination_btn = Button(frame, text="Choose Destination", command=CopyModel.destination_picker)
@@ -36,3 +38,6 @@ class GUIArchitect():
         start_button = Button(frame, text="Start")
         start_button.grid(sticky="EW")
 
+    def output_frame_config(self, frame):
+        title = Label(frame, text="Output Frame")
+        title.grid(sticky="EW")
