7b6bb505833312c9ded86bca8d1ed14cc714ffc2
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2025-01-27 16:50:03 +0330

    Configured input_frame

--- view/gui.py
+++ view/gui.py
@@ -26,18 +26,35 @@ class GUIArchitect():
         self.input_frame_config(input_frame)
         self.output_frame_config(output_frame)
 
-    def title_frame_config(self, frame):
+    def title_frame_config(self, frame: Frame):
         title = Label(frame, text="Copy Script", font=("Times New Roman", 25))
         title.grid(sticky="EW")
 
-    def input_frame_config(self, frame):
+    def input_frame_config(self, frame: Frame):
+        frame.rowconfigure(0, weight=1)
+        frame.rowconfigure(1, weight=1)
+        frame.rowconfigure(2, weight=1)
+        frame.columnconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        frame.columnconfigure(2, weight=1)
+        
+        origin_label = Label(frame, text="Origin:")
+        origin_label.grid(row=0, column=0)
+        origin_entry = Entry(frame)
+        origin_entry.grid(row=0, column=1, sticky="EW")
         choose_origin_btn = Button(frame, text="Choose Source", command=CopyModel.origin_picker)
-        choose_origin_btn.grid(sticky="EW")
+        choose_origin_btn.grid(row=0, column=2, sticky="EW")
+        
+        destination_label = Label(frame, text="Destination:")
+        destination_label.grid(row=1, column=0)
+        destination_entry = Entry(frame)
+        destination_entry.grid(row=1, column=1, sticky="EW")
         choose_destination_btn = Button(frame, text="Choose Destination", command=CopyModel.destination_picker)
-        choose_destination_btn.grid(sticky="EW")
+        choose_destination_btn.grid(row=1, column=2, sticky="EW")
+        
         start_button = Button(frame, text="Start")
-        start_button.grid(sticky="EW")
+        start_button.grid(row=2, column=1, sticky="EW")
 
-    def output_frame_config(self, frame):
+    def output_frame_config(self, frame: Frame):
         title = Label(frame, text="Output Frame")
         title.grid(sticky="EW")
